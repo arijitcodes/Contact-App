e3135258c6776c66e260f9bdd3b20f1e108d5725
Arijit Banerjee <72866688+arijitcodes@users.noreply.github.com>
2020-12-06 14:48:55 +0530

    Heroku Setup

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const path = require("path");
 
 // Importing DB Connection Method from DB Config
 const connectDB = require("./config/db");
@@ -25,6 +26,16 @@ app.use("/api/contacts", require("./routes/contacts"));
 
 // Connecting MongoDB
 connectDB();
+
+// Serve static assets in Production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 // Setting up server port
 const PORT = process.env.PORT || 5000;
